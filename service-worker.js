d64d370f1853876af5ac8f34cc5e39a06d8f8a5a
aman02deep <aman_02_deep@yahoo.com>
2016-09-19 08:18:43 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -8,6 +8,16 @@ var filesToCache = [
   '/pwa-sample/myPrime.json'
 ];
 
+self.addEventListener('push', function(event) {  
+  console.log('Received a push message', event);
+  var notificationOptions = {
+    body: 'Hello everybody!',
+    //icon: './images/hipstercat.jpg',
+    tag: 'simple-push-demo-notification'
+  };
+  return self.registration.showNotification('Important message', notificationOptions);
+}); 
+
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
   e.waitUntil(
