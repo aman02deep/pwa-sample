8febd29b0f713166f7198c3a7a8229a5f4baceb2
aman02deep <aman_02_deep@yahoo.com>
2016-09-28 16:03:23 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -12,12 +12,11 @@ var filesToCache = [
 ];
 
 self.addEventListener('push', function(event) {
- 
   console.log('Received a push message', event);
   var notificationOptions = {
     body: 'Hello everybody!',
-    //icon: './images/hipstercat.jpg',
-    tag: 'simple-push-demo-notification'
+    icon: '/pwa/sample/favicon.ico',
+    //tag: 'simple-push-demo-notification'
   };
   return self.registration.showNotification('Important message', notificationOptions);
 }); 
